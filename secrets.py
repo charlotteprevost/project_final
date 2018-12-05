8e7ab5eac600f806adc80708056d535ea3f16092
Charlotte Prevost <chaprevost91@gmail.com>
2018-12-05 15:28:16 -0600

    Deploy Attempt 4

---
+++ secrets.py
@@ -0,0 +1,13 @@
+
+# Django Secret Key
+SECRET_KEY = '(5n=jbkqc$_1(n&tg_py0pw%@eao!4f0j(k=554!!rc-t5d7d$'
+
+# DB Password
+DATABASE_PASSWORD = 'showdown'
+
+# Spotify
+SPOTIPY_CLIENT_ID = 'c01e1b1ebb7a4e5481d01f146b4d3859'
+SPOTIPY_CLIENT_SECRET = '354654f76df3405684feeb75e00573ea'
+
+# Songkick
+SONGKICK_API_KEY = 'ELWDRGsyLHxRYfgH'
