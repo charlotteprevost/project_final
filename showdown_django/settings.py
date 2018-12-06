f1dbacc69945f2bb6535a23dbef7677d057aee44
Charlotte Prevost <chaprevost91@gmail.com>
2018-12-05 18:51:22 -0600

    debugging

--- showdown_django/settings.py
+++ showdown_django/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'showdown_django.urls'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES = [
     {
@@ -143,7 +142,7 @@ STATIC_URL = '/static/'
 # os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'build/static'),
+  os.path.join(BASE_DIR, 'charlotteprevost/project_final/build/static'),
 ]
 
 
