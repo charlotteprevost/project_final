3c3995b72408956360027ec5de27e7e73d745e53
Charlotte Prevost <chaprevost91@gmail.com>
2018-12-05 19:09:40 -0600

    debugging

--- showdown_django/settings.py
+++ showdown_django/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, '/build/static')
 
  # STATIC_TMP = os.path.join(BASE_DIR, 'build/static')
