e1aa41b42e3c86b99d94d0add972d1e00a49ca4a
Charlotte Prevost <chaprevost91@gmail.com>
2018-12-14 13:09:38 -0600

    Modified src index.js to show App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-require('./style.css');
+// require('./style.css');
 
-const favicon = require('./assets/favicon.png');
-let link = document.createElement('link');
-link.type = 'image/png';
-link.rel = 'shortcut icon';
-link.href = favicon;
-document.head.appendChild(link);
+// const favicon = require('./assets/favicon.png');
+// let link = document.createElement('link');
+// link.type = 'image/png';
+// link.rel = 'shortcut icon';
+// link.href = favicon;
+// document.head.appendChild(link);
 
-let title = document.createElement('h1');
-title.innerText = 'Hello Wepack';
-document.body.appendChild(title);
+// let title = document.createElement('h1');
+// title.innerText = 'Hello Wepack';
+// document.body.appendChild(title);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
